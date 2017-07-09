db5ad97310d9169e064495d710088780bced5233
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-09 20:42:03 +0200

    Added files via upload
    This is work in progress, so far i was able to understand and correct myself the mistakes i have made in the program during the tutorial.

---
+++ FoodItem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace APFoodEquals
+{
+    public enum FoodGroup { Meat, Fruit, Vegetables, Sweets }
+
+    //inheriting from IEquatable
+    //creating a generalized method measure equality of different instances
+    public struct FoodItem : IEquatable<FoodItem>
+    {
+        private readonly string _name;
+        private readonly FoodGroup _group;
+
+        public string Name { get { return _name; } }
+        public FoodGroup Group { get { return _group; } }
+
+        //constructor that sets necessary fields
+        public FoodItem(string Name, FoodGroup Group)
+        {
+            this._name = Name;
+            this._group = Group;
+        }
+
+        //converts an object to its string representation
+        //but we override it
+        public override string ToString()
+        {
+            return _name;
+        }
+
+        //overloading equality operator
+        //comparing left hand side fooditem to right hand side food item
+        //notice there is no type check, input parameters have to match exactly
+        public static bool operator ==(FoodItem lhs, FoodItem rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        //ovrloading the inequality operator (required is equality has the same)
+        public static bool operator !=(FoodItem lhs, FoodItem rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        //overriding GetHashCode because it works closely with equality
+        //therefore if equality is overriden than so should GetHasCode();
+        // ^ = the exclusive OR operator
+        public override int GetHashCode()
+        {
+            return _name.GetHashCode() ^ _group.GetHashCode();
+        }
+
+        //comparing different instances by using IEquatable properties
+        public bool Equals(FoodItem other)
+        {
+            return this._name == other._name && this._group == other._group;
+        }
+
+        //attempting to perform the same as "public bool Equals" from above
+        public override bool Equals(object obj)
+        {
+            //checking that object passed as a parameter is a food item
+            //because of the boxing this method is still much slower than IEquatable
+            if (obj is FoodItem)
+                return Equals((FoodItem)obj);
+            else
+                return false;
+        }
+    }
+}    
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APFoodEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiated 3 food items
+            FoodItem banana = new FoodItem("banana", FoodGroup.Fruit);
+            FoodItem banana2 = new FoodItem("banana", FoodGroup.Fruit);
+            FoodItem chocolate = new FoodItem("chocolate", FoodGroup.Sweets);
+
+            //comparing each food item in turn
+            //we expect banana to equal to banana2 because they have same description
+            //and same food group (recall overriding equalities in "FoodItem.cs"
+            Console.WriteLine("banana == banana2: " + (banana == banana2));
+            Console.WriteLine("banana2 == chocolate: " + (banana2 == chocolate));
+            Console.WriteLine("chocolate == banana: " + (chocolate == banana));
+        }
+    }
+}
